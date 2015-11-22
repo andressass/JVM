af31257f1f3cf60d9ffde2ff7d51df55635ded59
Rondinele Prado <rondinele_prado@gmx.com>
2015-11-22 11:36:20 -0200

    Definição do método loadClass na interface do ClassLoader

--- JavaVirtualMachine/JavaVirtualMachine/include/ClassLoader/I_CLASSLOADER.h
+++ JavaVirtualMachine/JavaVirtualMachine/include/ClassLoader/I_CLASSLOADER.h
@@ -10,6 +10,24 @@
 
 #ifndef I_CLASSLOADER_h
 #define I_CLASSLOADER_h
+#ifdef M_CLASSLOADER_SERV
+#define EXT9
+#else
+#define EXT9 extern
+#endif
+
+#include "../Estruturas/E_JVM.h"
+
+
+//--------------------------------------------------------------------------------------------------
+/*!
+ * Metodo que, dado um nome qualificado de classe, retorna uma estrutura JavaClass preenchida
+ *
+ * \param qualifiedName Nome qualificado da classe a ser carregada
+ * \return Estrutura arqClass preenchida com os dados da classe carregada 
+ */
+EXT9 JavaClass* loadCLass(const char* qualifiedName);
+
 
 //--------------------------------------------------------------------------------------------------
 
